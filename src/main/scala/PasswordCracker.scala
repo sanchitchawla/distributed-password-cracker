497470f1f803d2cd0987e0aebd527760f101da34
Sanchit Chawla <sanchit_sid@hotmail.com>
2018-01-25 16:41:13 +0700

    Local Cracker works

--- src/main/scala/PasswordCracker.scala
+++ src/main/scala/PasswordCracker.scala
@@ -5,24 +5,51 @@ import org.apache.commons.codec.digest.Crypt
   */
 object PasswordCracker {
 
+  val all_chars: List[Char] = (('A' to 'Z') ++ ('a' to 'z') ++ ('0' to '9')).toList
+
   def cracker(startRange: String, endRange: String, hash:String): Boolean ={
     def helper(curr: String, found: Boolean): Boolean = {
-      if (getHashed(curr) == hash) true
+      if (getHashed(curr).equals(hash)) true
+      else if (curr.equals(endRange)) false
       else {
         helper(getNext(curr), found)
       }
     }
-    if (startRange == endRange) false else helper(startRange, found = false)
+    helper(startRange, found = false)
   }
 
   def getHashed(curr: String): String = Crypt.crypt(curr, "ic")
-  def getNext(curr: String): String = {
-    val chars = ('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')
-    "Sunny is very cool"
+
+  def nextChar(c: Char): Char = {
+    val cur_i = all_chars.indexOf(c)
+    all_chars((cur_i+1)%all_chars.length)
   }
+
+  def flip(s: List[Char]): List[Char] = {
+    val n = s.length-1
+
+    def helper(s: List[Char], i: Int): List[Char] = {
+      if(i < 0) {
+        'A' :: s
+      }
+      else{
+        val next_c = nextChar(s(i))
+        var new_s = s.updated(i, next_c)
+        if(new_s(i) == 'A') new_s = helper(new_s,i-1)
+
+        new_s
+      }
+    }
+    helper(s,n)
+  }
+
+  def getNext(inputString: String): String = {
+    flip(inputString.toList).mkString
+  }
+
   def main(args: Array[String]): Unit = {
-    val chars = ('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')
-    println(chars)
+    println(cracker("A", "AAAAD", "icMez.omxHuqU"))
+    println(Crypt.crypt("9999", "ic"))
   }
 
 }
